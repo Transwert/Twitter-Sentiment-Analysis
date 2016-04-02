cf36ebb25e8cf00734b6062234ad28b1843e6243
oikobill <economou.vasilis7@gmail.com>
2016-04-02 10:46:40 -0700

    everything working

--- get_all_tweets.py
+++ get_all_tweets.py
@@ -1,6 +1,10 @@
 import tweepy
 import csv
 
+key = 'UoOP5xpzbFqCLm3WKEq6VqhPY'
+secret = 'RUPDwPP4cdYvCkfkuOZcX4OofCXcsZRcXxN6BUIoBkYTddVdCj'
+access = '715768598367715329-6QKfskhjRLrC6zmoIHllvL7SXISd522'
+a_secret = 'RouBCcG7hHI4eA0MoaRtiGfX2nvFKoNEvrrAoczhttTGc'
 
 auth = tweepy.OAuthHandler( key, secret)
 auth.set_access_token( access, a_secret)
@@ -9,21 +13,55 @@ auth.set_access_token(access, a_secret)
 api = tweepy.API(auth)
 
 #returns a list containing at most 2000 tweets from a particular person's timeline
+#return format: list of strings
+# status: Working
 def get_all_tweets(screen_name):
-	tweets = []
+	tweets = []	
+	
+	#make initial request for most recent tweets (200 is the maximum allowed count)
+	new_tweets = api.user_timeline(screen_name = screen_name,count=200) 
 
-	to_add = get_once(screen_name)
+	tweets.extend(new_tweets)
 
-	for i in range(2):
-		tweets.extend(to_add)
-		if len(to_add) > 0:
-			break
-		to_add = get_once(screen_name)
+	last_tweet = tweets[-1].id - 1
+	count = 0
+	while len(new_tweets) > 0 and count < 2: # change 2 to 10
+		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=last_tweet)
+		tweets.extend(new_tweets)
+		last_tweet = tweets[-1].id - 1
+		count += 1
+	tweets = [tweet.text.encode("utf-8") for tweet in tweets]
 	return tweets
 
-def get_once(screen_name):
-	text = [i.text for i in api.user_timeline(screen_name, count = 200)]
-	return text
+# maps media outlet's screen_name to a list of urls
+# info will be used to do sentiment analysis on the article of that link
+# status: working
+def map_outlet_to_url(lst):
+	d = {}
+	for media in lst:
+		tweets = get_all_tweets(media) 
+		tweets = split_text(tweets)
+		urls = find_url(tweets)
+		d[media] = urls
+	return d
+
+# returns a list of all the urls that appears in the list of tweets
+def find_url(tweet):
+	lst = []
+	for word in tweet:
+		if word[0:8] == b'https://':
+			lst.append(word)
+	return lst
+# returns a split version of the text into one big array for ease of use
+def split_text(tweets):
+	words = []
+	for i in tweets:
+		for j in i.split():
+			words.append(j)
+	return words 
+
+
+
 
 # converts the list from get_all_tweets to a csv
 def list_to_csv(lst):

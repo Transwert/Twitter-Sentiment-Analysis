1f3aea9b4502ce2ef634742ac10e349f5b69d5df
divyansh_agarwal <divyanshagarwal97@gmail.com>
2016-04-28 23:22:56 -0700

    classifier accuracy testing code added

--- classifier_accuracy.py
+++ classifier_accuracy.py
@@ -11,16 +11,17 @@ def both_sets(reader):
 	test_set = []
 	training_data = []
 	total_count = 0
+	test_count = 0
 	for row in reader:
-		if random.random() < 0.5:
-			test_set.append(row[1])
-		training_data.append(row)
-		total_count += 1
+		if random.random() < 0.2:
+			if random.random() < 0.5:
+				test_set.append(row)
+				test_count += 1
+			training_data.append(row)
+			total_count += 1
 		
-
-	return test_set, training_data, total_count
-
-#def check_accuracy(fileName):
+	#print(total_count)
+	return test_set, training_data, total_count, test_count
 
 with codecs.open('training_set_better.csv', encoding='utf-8', errors='replace') as f:
 	reader = csv.reader(f)
@@ -28,20 +29,26 @@ with codecs.open('training_set_better.csv', encoding='utf-8', errors='replace') 
 
 	percentage_list = []
 
+	classify = classifier()
+
+
 	for i in range(0, 500):
-		test_set, cross_validation_set, total_count = both_sets(reader)
+		test_set, training_data, total_count, test_count = both_sets(reader)
 
 		count_right = 0
 
-		classify = classifier()
+		print(total_count)
+		if (test_count != 0):
+			
+			for i in range(0, test_count):
+				if (test_set[i][0] == classify(test_set[i][1])):
 
-		for i in range(0, len(test_set)):
-			if (test_set[i][0] == classify(cross_validation_set[i])):
-				count_right += 1
+					count_right += 1
 
-		percent_right = count_right * 100
-		percentage_list.append(percent_right)
+			percent_right = (count_right/test_count) * 100
+			percentage_list.append(percent_right)
 
-	plt.hist(percentage_list)
+	print(percentage_list)
+	plt.hist(percentage_list, bins = 20)
 	plt.show()
 
